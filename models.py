8f22252438796cac4f2db283b4a073750772f515
Zachary92 <126645101+Zachary92@users.noreply.github.com>
2023-05-31 10:55:21 +0800

    Add files via upload

--- models.py
+++ models.py
@@ -11,7 +11,8 @@ from dv import cars
 import warnings
 warnings.filterwarnings('ignore')
 
-car_features=['horsepower','enginesize','curbweight','carwidth','carlength','highwaympg','citympg']
+car_features=['horsepower','enginesize','curbweight',
+              'carwidth','carlength','highwaympg','citympg']
 x=cars[car_features]
 y=cars.price
 train_x,test_x,train_y,test_y=train_test_split(x,y,random_state=0)
@@ -28,7 +29,9 @@ linear_predict_output=pd.DataFrame({'Horsepower':test_x.horsepower,'Enginesize':
                                   'Curbweight':test_x.curbweight,'SalePrice':linear_predict})
 
 #polynomial model using ridge
-Input_poly=[('scaler',StandardScaler()),('polynomial',PolynomialFeatures(degree=2,include_bias=False)),('model',Ridge(alpha=0.1))]
+Input_poly=[('scaler',StandardScaler()),
+            ('polynomial',PolynomialFeatures(degree=2,
+            include_bias=False)),('model',Ridge(alpha=0.1))]
 pipe_poly=Pipeline(Input_poly)
 pipe_poly.fit(train_x,train_y)
 polynomial=pipe_poly.score(test_x,test_y)
@@ -42,12 +45,14 @@ poly_predict_output=pd.DataFrame({'Horsepower':test_x.horsepower,'Enginesize':te
 car_brand2=pd.get_dummies(cars['car_brand'],drop_first=True)
 car_model2=pd.get_dummies(cars['car_model'],drop_first=True)
 fuel_type2=pd.get_dummies(cars['fueltype'],drop_first=True)
-X=pd.concat([car_brand2,car_model2,fuel_type2,cars[['highwaympg','citympg']]],axis=1)
-Y=cars['price']
+X=pd.concat([car_brand2,car_model2,
+             fuel_type2,cars[['highwaympg','citympg']]],axis=1)
+Y=cars.price
 train_X,test_X,train_Y,test_Y=train_test_split(X,Y,random_state=0)
 
 #decision tree model
-Input1=[('scaler',StandardScaler()),('model',DecisionTreeRegressor())]
+Input1=[('scaler',StandardScaler()),
+        ('model',DecisionTreeRegressor())]
 pipe1=Pipeline(Input1)
 pipe1.fit(train_X,train_Y)
 decision_tree=pipe1.score(test_X,test_Y)
@@ -61,7 +66,8 @@ dt_predict_output=pd.DataFrame({'Car_Brand':car_brand2.columns[test_X.iloc[:,0].
                                 'Sales_Price':decision_tree_predict})
 
 #random forest model 
-Input2=[('scaler',StandardScaler()),('model',RandomForestRegressor())]
+Input2=[('scaler',StandardScaler()),
+        ('model',RandomForestRegressor())]
 pipe2=Pipeline(Input2)
 pipe2.fit(train_X,train_Y)
 random_forest=pipe2.score(test_X,test_Y)
@@ -76,7 +82,8 @@ rf_predict_output=pd.DataFrame({'Car_Brand':car_brand2.columns[test_X.iloc[:,0].
 
 #R squared score for each model for evaluating
 rscore_results=pd.DataFrame({'Linear':linear,'Polynomial': polynomial,
-                             'DecisionTree':decision_tree,'RandomForest':random_forest},index=[0])
+                             'DecisionTree':decision_tree,
+                             'RandomForest':random_forest},index=[0])
 print(rscore_results)
 print(poly_predict_output.head())
 print(rf_predict_output.head())
